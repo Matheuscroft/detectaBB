370455757f5666ddda208b31d0d2bd415299787a
David-Evangelist <dev.davidwilliam@gmail.com>
2025-05-16 14:07:41 -0300

    "feat: adiciona tela de recuperação de senha conforme Figma"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,13 +12,18 @@ const routes: Routes = [
   {
     path: 'home-page',
     loadChildren: () => import('./pages/home-page/home-page.module').then( m => m.HomePagePageModule)
-  },  {
+  },
+  {
     path: 'upload',
     loadChildren: () => import('./pages/upload/upload.module').then( m => m.UploadPageModule)
   },
   {
     path: 'result',
     loadChildren: () => import('./pages/result/result.module').then( m => m.ResultPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
   },
 
 ];
